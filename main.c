541f30db0e0fccb86de85b3aeef0b0c58ae70289
Alex Stråe <alex.strae@me.com>
2026-03-31 15:20:19 +0200

    second commit

---
+++ main.c
@@ -0,0 +1,80 @@
+#include "gd32vf103.h"
+#include "drivers.h"
+#include "adc.h"
+#include "lcd.h"
+#include "usart.h"
+#define EI 1
+#define DI 0
+
+void rtcInit(void){
+   
+   rcu_periph_clock_enable(RCU_PMU); // enable power managemenet unit - perhaps enabled by default
+   // enable write access to the registers in the backup domain
+   pmu_backup_write_enable();
+   // enable backup domain
+   rcu_periph_clock_enable(RCU_BKPI);
+   // reset backup domain registers
+   bkp_deinit();
+   // set the results of a previous calibration procedure
+   // bkp_rtc_calibration_value_set(x);
+
+   // setup RTC
+   // enable external low speed XO
+   //rcu_osci_on(RCU_LXTAL);
+   if (rcu_osci_stab_wait(RCU_HXTAL)) {
+     // use external low speed oscillaotr, i.e. 32.768 kHz
+     rcu_rtc_clock_config(RCU_RTCSRC_HXTAL_DIV_128);
+     rcu_periph_clock_enable(RCU_RTC);
+     // wait until shadow registers are synced from the backup domain
+     // over the APB bus
+     rtc_register_sync_wait();
+     // wait until shadow register changes are synced over APB
+     // to the backup doamin
+     rtc_lwoff_wait();
+     // prescale to 1 second
+     rtc_prescaler_set(62500 - 1);
+     rtc_lwoff_wait();
+     rtc_flag_clear(RTC_INT_FLAG_SECOND);
+     //rtc_interrupt_enable(RTC_INT_SECOND);
+     rtc_lwoff_wait();
+   }
+}
+
+int main(void){
+    int ms=0, s=0, key, pKey=-1, c=0, idle=0, rtc, hh, mm, ss;
+    int lookUpTbl[16]={1,4,7,14,2,5,8,0,3,6,9,15,10,11,12,13};
+    int dac=0, speed=-100;
+    int adcr, tmpr;
+
+
+    t5omsi();                               // Initialize timer5 1kHz
+    colinit();                              // Initialize column toolbox
+    l88init();                              // Initialize 8*8 led toolbox
+    keyinit();                              // Initialize keyboard toolbox
+    ADC3powerUpInit(1);                     // Initialize ADC0, Ch3
+    Lcd_SetType(LCD_INVERTED);                // or use LCD_INVERTED!
+    Lcd_Init();
+    LCD_Clear(BLACK);
+    
+    //rtcInit();                              // Initialize RTC
+    //rtc_counter_set(3600+60+1);
+    u0init(EI);                               // Initialize USART0 toolbox
+    //eclic_global_interrupt_enable();          // !!! INTERRUPT ENABLED !!!
+
+    while (1) {
+        idle++;                               // Manage Async events
+
+        if (t5expq()) {                         // Manage periodic tasks
+            l88row(colset());                   // ...8*8LED and Keyboard
+            ms++;                               // ...One second heart beat
+            if (ms==1000){
+              ms=0;
+              LCD_ShowStr(0, 0, "Plantii v0.1", WHITE, TRANSPARENT);
+            }
+            l88mem(2,idle>>8);                  // ...Performance monitor
+            l88mem(3,idle); idle=0;
+            adc_software_trigger_enable(ADC0,   // Trigger another ADC conversion!
+                                        ADC_REGULAR_CHANNEL);
+        }
+    }
+}
